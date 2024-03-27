04965eeda700bbac42661319c0084742d1a7793d
Milton Chang <mchangpi@gmail.com>
2024-03-27 18:02:14 +0800

    Add cron.js to wake up Render.com App

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ import authRoutes from './routes/auth.route.js';
 
 import connectMongoDB from './db/connectMongoDB.js';
 
+import job from "./cron.js";
+
 dotenv.config();
+
+job.start();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
